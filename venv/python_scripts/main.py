e33ef8c3587969efb584b87f0881e91ed6410ab3
ThaProgramah <m.pauksch83@gmail.com>
2024-09-04 07:50:49 +0200

    Update main.py
    main page with menu and the Function Extraction as class

--- venv/python_scripts/main.py
+++ venv/python_scripts/main.py
@@ -3,9 +3,15 @@ from pprint import pprint
 from pydantic import BaseModel, Field
 from typing import Literal, List, Dict, NotRequired
 
-from python_scripts.function_calling.llm_function_call import file_finder
-from python_scripts.function_calling.llm_function_call import function_finder
-from python_scripts.function_calling.llm_function_call import function_information_extraction
+#--------------------------------------------------------------------------------------------------------------
+# Files
+
+from python_scripts.function_calling.llm_function_call import FunctionExtraction
+
+#--------------------------------------------------------------------------------------------------------------
+# External Libraries here
+
+from duckduckgo_search import DDGS
 
 #--------------------------------------------------------------------------------------------------------------
 
@@ -16,21 +22,40 @@ class MessageTemplate(BaseModel):
 #--------------------------------------------------------------------------------------------------------------
 
 class Agent:
-    def __init__(self):
-        self.llm_path = r"venv/model/mistral-7b-instruct-v0.2.Q6_K.gguf"
+    def __init__(self, tools=None):
+        self.llm_path = r"C:\Users\mpauk\Desktop\python_programming\llm_rag\venv\models\mistral-7b-instruct-v0.2.Q6_K.gguf"
         self.messages = []
+        self.tools = tools if tools is not None else ""
 
     def model_exec(self, question):
         model = Llama(model_path=self.llm_path, chat_format="mistral-instruct", n_gpu_layers=50)
+
         message = MessageTemplate(role="user", content=question)
         message_dict = message.model_dump()
-        output = model.create_chat_completion(model=model, messages=[message_dict],
-                                              functions=function_information_extraction())
+
+        external_files = FunctionExtraction.file_finder(self.tools)
+        external_functions = FunctionExtraction.function_finder(external_files)
+        toolbox = FunctionExtraction.function_information_extraction(external_functions)
+
+        #print("TOOLBOX: ", toolbox)
+
+        output = model.create_chat_completion(model=model, messages=[message_dict], functions=toolbox)
         return output["choices"][0]["message"]['content']
     
 #--------------------------------------------------------------------------------------------------------------
 # Menu
 
+counter = 0
 model = Agent()
-message = model.model_exec("What is Sandmänchen?")
-pprint(message)
+while True:
+    counter += 1
+    print("-------------------------------------------------------------------------")
+    print(f"Enter your Question or (q)uit: ")
+    user_query = input(f"In[{counter}]: ")
+    user_query = user_query.lower()
+    if user_query == "q":
+        print("Thanks for your Questions!")
+        break
+    else:
+        message = model.model_exec(user_query)
+        print(f"Out[{counter}]: {message}")
